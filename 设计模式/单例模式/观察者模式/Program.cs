f9650e37963c8a1d55fc3016b3db3ea286e30ab4
黎峰 喻 <1085937760@qq.com>
2018-06-17 13:15:06 +0800

    2

--- 设计模式/单例模式/观察者模式/Program.cs
+++ 设计模式/单例模式/观察者模式/Program.cs
@@ -4,27 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace 观察者模式
+namespace 观察者模式订阅者模式
 {
-    
-        interface Message_Kinds
-        {
+    #region 抽象通知者
+    /// <summary>
+    /// 通知者
+    /// </summary>
+    interface IMessage_Kinds
+        {/// <summary>
+        /// 添加订阅者
+        /// </summary>
+        /// <param name="customer"></param>
             void Attach(Customer customer);
+        /// <summary>
+        /// 删除用户
+        /// </summary>
+        /// <param name="customer"></param>
             void Detach(Customer customer);
+        /// <summary>
+        /// 通知
+        /// </summary>
             void Notify();
+           
             string Message_kindsState
             {
                 get;
                 set;
             }
         }
-
-        abstract class Customer
+    #endregion
+    #region 抽象用户
+    /// <summary>
+    /// 抽象用户
+    /// </summary>
+    abstract class Customer
         {
-            protected string name;
-            protected Message_Kinds Kinds;
+        protected string name;
+        protected IMessage_Kinds Kinds;
 
-            public Customer(string name, Message_Kinds Kinds)
+        public void SetKinds(IMessage_Kinds kinds)
+        {
+            Kinds.Detach(this);
+            Kinds = kinds;
+            kinds.Attach(this);
+        }
+        public Customer(string name, IMessage_Kinds Kinds)
             {
                 this.name = name;
                 this.Kinds = Kinds;
@@ -33,18 +57,19 @@ namespace 观察者模式
             public abstract void Update();
         }
 
-
-        #region 普通会员
-        //普通会员
-        class Membercustomer : Customer
+    #endregion
+    #region 普通会员
+    //普通会员
+    class Membercustomer : Customer
         {
-            public Membercustomer(string name, Message_Kinds Kinds)
+            public Membercustomer(string name, IMessage_Kinds Kinds)
                 : base(name, Kinds)
             {
+                Kinds.Attach(this); 
             }
             public override void Update()
             {
-                Console.WriteLine("{0};普通会员{1}收到通知！", Kinds.Message_kindsState, name);
+                Console.WriteLine("{0};普通会员{1}收到通知！", Kinds.Message_kindsState,name);
             }
         }
         #endregion
@@ -52,8 +77,9 @@ namespace 观察者模式
         #region  VIP会员
         class VIPcustomer : Customer
         {
-            public VIPcustomer(string name, Message_Kinds Kinds) : base(name, Kinds)
+            public VIPcustomer(string name, IMessage_Kinds Kinds) : base(name, Kinds)
             {
+                 Kinds.Attach(this);
             }
             public override void Update()
             {
@@ -62,48 +88,45 @@ namespace 观察者模式
         }
         #endregion
 
-        class Productname : Message_Kinds
+        class Productname : IMessage_Kinds
         {
-            #region 会员列表
+        /// <summary>
+        /// 订阅者
+        /// </summary>
             private IList<Customer> customers = new List<Customer>();
-            #endregion
             private string action;
             public string name;
-            #region 增加连接
             public void Attach(Customer customer)
             {
                 customers.Add(customer);
             }
-            #endregion
-            #region 减少
             public void Detach(Customer customer)
             {
                 customers.Remove(customer);
             }
-            #endregion
-            #region  通知消息
             public void Notify()
             {
                 foreach (Customer o in customers)
                     o.Update();
             }
             //名字信息
-            #endregion
             public string Message_kindsState
             {
                 get { return action; }
                 set { action = value; }
             }
             //设置商品名
-            public void setname(string name)
+            public void Setname(string name)
             {
                 this.name = name;
+                Message_kindsState = "商品名已经更改为:" +name;
+                Notify();
             }
 
         }
 
 
-        class Productprice : Message_Kinds
+        class Productprice : IMessage_Kinds
         {
             //会员列表
             private IList<Customer> customers = new List<Customer>();
@@ -134,7 +157,9 @@ namespace 观察者模式
             //设置价格
             public void setprice(double price)
             {
-                this.price = price;
+               this.price = price;
+               Message_kindsState = "商品价格已经更改为:" + price;
+               Notify();
             }
 
         }
@@ -144,9 +169,9 @@ namespace 观察者模式
         {
 
             Productname name = new Productname();
-            name.setname("皮肤");
+
             Productprice price = new Productprice();
-            price.setprice(50);
+
 
             //普通会员
             Membercustomer member1 = new Membercustomer("小明", name);
@@ -156,23 +181,12 @@ namespace 观察者模式
             Membercustomer member2 = new Membercustomer("大基佬", price);
             //VIP会员
             VIPcustomer VIP2 = new VIPcustomer("小狼", price);
-
-            name.Attach(member1);
-            name.Attach(VIP1);
-
-            price.Attach(member2);
-            price.Attach(VIP2);
-
-            //商品名更改
-            name.Message_kindsState = "商品名已经更改为:" + name.name;
-            //发出通知
-            name.Notify();
-
-            //价格更改
-            price.Message_kindsState = "商品价格已经更改为:" + price.price;
-            //发出通知
-            price.Notify();
-
+            name.Setname("皮");
+            price.setprice(5);
+            VIP1.SetKinds(price);
+            VIP2.SetKinds(name);
+            name.Setname("皮肤");
+            price.setprice(50);
             Console.Read();
         }
     }
---
+++ 设计模式/单例模式/观察者模式订阅者模式/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 观察者模式订阅者模式
+{
+    /// <summary>
+    /// 行为抽象
+    /// </summary>
+    public abstract class IMessage_Kinds
+    {
+        protected IList<IMessage_Kinds> customers = new List<IMessage_Kinds>();
+        protected int Id { get; set; }//id
+        protected string Name { get; set; }//name
+        protected string Message_kindsState;//message
+
+        public string GetMessage()
+        {
+            return Message_kindsState;
+        }
+        public void SetMessage(string srting)
+        {
+            Message_kindsState = srting;
+        }
+
+        /// <summary>
+        /// 添加订阅者
+        /// </summary>
+        /// <param name="customer"></param>
+        public void Attach(IMessage_Kinds message_Kinds)
+        {
+            customers.Add(message_Kinds);
+        }
+        /// <summary>
+        /// 删除用户
+        /// </summary>
+        /// <param name="customer"></param>
+        public void Detach(IMessage_Kinds message_Kinds)
+        {
+            customers.Add(message_Kinds);
+        }
+        /// <summary>
+        /// 通知
+        /// </summary>
+        public abstract void Notify();
+        /// <summary>
+        /// 通知信息改变
+        /// </summary>
+        public abstract void Update(MesssageBW messsageBW);
+    }
+
+    public class MesssageBW
+    {
+        private IMessage_Kinds _orgin;
+        private IMessage_Kinds _aims;
+
+        public MesssageBW(IMessage_Kinds orgin, IMessage_Kinds aims)
+        {
+            _orgin = orgin;
+            _aims = aims;
+        }
+        public IMessage_Kinds GetOrgin()
+        {
+            return _orgin;
+        }
+    }
+    interface IAction
+    {
+        void SetAction(object ob);
+    }
+    #region 抽象用户
+    /// <summary>
+    /// 抽象用户
+    /// </summary>
+    abstract class Customer:IMessage_Kinds
+    {
+        public void AddKinds(IMessage_Kinds kinds)
+        {
+            this.Attach(kinds);
+            kinds.Attach(this);
+            //message
+            SetMessage(Id+":"+Name + "订阅了你！");
+            kinds.Update(new MesssageBW(this,kinds));
+        }
+        public void DelKinds(IMessage_Kinds kinds)
+        {
+            this.Detach(kinds);
+            kinds.Detach(kinds);
+            SetMessage(Id+":"+Name + "取消了订阅！");
+            kinds.Update(new MesssageBW(this,kinds));
+        }
+        public Customer(int id,string _name,params IMessage_Kinds[] messages)
+        {
+            Id = id;
+            Name = _name;
+            foreach (IMessage_Kinds kind in messages)
+            {
+                AddKinds(kind);
+            }
+        }
+
+    }
+
+    #endregion
+    #region 普通会员
+    //普通会员
+    class Membercustomer : Customer
+    {
+        public Membercustomer(int id, string name, params IMessage_Kinds[] kinds)
+            : base(id,name, kinds)
+        {
+
+        }
+
+        public override void Notify()
+        {
+            //pass
+        }
+
+        public override void Update(MesssageBW messsage)
+        {
+            Console.WriteLine("{0};普通会员{1}收到通知！", messsage.GetOrgin().GetMessage(), Name);
+        }
+    }
+    #endregion
+
+    #region  VIP会员
+    class VIPcustomer : Customer
+    {
+        public VIPcustomer(int id,string name, params IMessage_Kinds[] kinds) : base(id,name,kinds)
+        {
+        }
+
+        public override void Notify()
+        {
+           //pass
+        }
+
+        public override void Update(MesssageBW messsage)
+        {
+            Console.WriteLine("{0};VIP会员{1}收到通知！", messsage.GetOrgin().GetMessage(), Name);
+        }
+    }
+    #endregion
+
+     abstract  class Product : IMessage_Kinds,IAction
+    {
+        public Product(int _id,string _name,string _action)
+        {
+            Id = _id;
+            Name = _name;
+            Action = _action;
+        }
+
+        protected string Action { get; set; }
+
+        public override void Notify()
+        {
+            foreach (Customer o in customers)
+                o.Update(new MesssageBW(this,o));
+        }
+
+        public abstract void SetAction(object ob);
+    }
+
+
+    class Productprice : Product
+    {
+         public  Productprice(int _id,string _name,string _action) : base(_id, _name, _action)
+        {
+        }
+         private  double _price;
+
+        //设置价格
+        public  override void  SetAction(object ob)
+        {
+            _price = Convert.ToDouble(ob);
+            Message_kindsState = "商品价格已经更改为:" + _price;
+            Notify();
+        }
+
+        public override void Update(MesssageBW messsageBW)
+        {
+            Console.WriteLine("{0};{1}收到通知！", messsageBW.GetOrgin().GetMessage(), Name);
+        }
+    }
+    class Productname : Product
+    {
+        public Productname(int _id, string _name, string _action) : base(_id, _name, _action)
+        {
+        }
+        private double _name;
+
+        //设置价格
+        public override void SetAction(object ob)
+        {
+            _name = Convert.ToDouble(ob);
+            Message_kindsState = "商品名字已经更改为:" + _name;
+            Notify();
+        }
+
+        public override void Update(MesssageBW messsageBW)
+        {
+            Console.WriteLine("{0};{1}收到通知！", messsageBW.GetOrgin().GetMessage(), Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product kinds = new Productprice(1, "人民日报", "price");
+            IMessage_Kinds VP1 = new VIPcustomer(01, "小明", kinds);
+            IMessage_Kinds men1 = new Membercustomer(02, "大基佬", kinds);
+            kinds.SetAction(11.2);
+            Console.Read();
+        }
+    }
+}
